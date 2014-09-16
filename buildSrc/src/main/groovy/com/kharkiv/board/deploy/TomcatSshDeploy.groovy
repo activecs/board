fa2ea8e0ec705998a5998088199d6a6016759ea9
Eduard Dereza <eduard.dereza@travisperkins.co.uk>
2014-09-16 18:45:46 +0300

    Small fixes for deploy task
    Rapid Rabbits team

--- buildSrc/src/main/groovy/com/kharkiv/board/deploy/TomcatSshDeploy.groovy
+++ buildSrc/src/main/groovy/com/kharkiv/board/deploy/TomcatSshDeploy.groovy
@@ -1,36 +1,43 @@
 package com.kharkiv.board.deploy
 
+import com.google.common.collect.Lists
+import com.google.common.collect.Sets
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
+import org.hidetake.gradle.ssh.plugin.Remote;
+import org.gradle.api.tasks.bundling.War;
 
 class TomcatSshDeploy extends DefaultTask{
 
     @Input
-    Object remote;
+    Remote remote;
     @Input
     String tomcatPath;
     @Input
     String warName;
     @Input
-    String warAbsolutePath;
+    boolean runAsRoot;
+    @Input
+    War warTask;
+
 
     TomcatSshDeploy(){
         description = 'Deploys application to remote hosting';
         group = 'Remote deploying';
-        dependsOn: 'war';
     }
 
     @TaskAction
     void deploy(){
+        String sudo = runAsRoot ? 'sudo' : '';
         project.convention.plugins.ssh.sshexec({
             session(remote) {
-                execute "sh ${tomcatPath}/bin/shutdown.sh"
-                execute "rm -f ${tomcatPath}/webapps/${warName}.war"
-                execute "rm -rf ${tomcatPath}/webapps/${warName}"
-                put     warAbsolutePath, "${tomcatPath}/webapps/${warName}.war"
-                execute "sh ${tomcatPath}/bin/startup.sh"
+               execute "${sudo} sh ${tomcatPath}/bin/shutdown.sh"
+               execute "${sudo} rm -f ${tomcatPath}/webapps/${warName}.war"
+               execute "${sudo} rm -rf ${tomcatPath}/webapps/${warName}"
+               put     warTask.archivePath.absolutePath, "${tomcatPath}/webapps/${warName}.war"
+               execute "${sudo} sh ${tomcatPath}/bin/startup.sh"
             }
         })
     }
