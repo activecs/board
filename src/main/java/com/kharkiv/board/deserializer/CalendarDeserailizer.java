422cf72af50b639fe3e0c86ab350a5b9f22ad2a9
Eduard Dereza <eduard.dereza@yandex.ru>
2014-10-10 20:25:16 +0300

    Fixed one violation
    KAA team

--- src/main/java/com/kharkiv/board/deserializer/CalendarDeserailizer.java
+++ src/main/java/com/kharkiv/board/deserializer/CalendarDeserailizer.java
@@ -9,14 +9,13 @@ import java.util.Date;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
 
 public class CalendarDeserailizer implements JsonDeserializer<Calendar> {
 
 	private static final String DATE_FORMAT = "dd.MM.yyyy - hh:mm";
 
 	@Override
-	public Calendar deserialize(JsonElement json, Type typeOfT,JsonDeserializationContext context) throws JsonParseException {
+	public Calendar deserialize(JsonElement json, Type typeOfT,JsonDeserializationContext context) {
 		try {
 			SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
 			Date date = formatter.parse(json.getAsString());
